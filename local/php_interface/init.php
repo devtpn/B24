2749b54236e054b8a94ca59aa2b3942c12c6743e
Павел <tpn@MacBook-Pro-Anastasia.local>
2026-04-29 15:43:29 +0500

    debug: логирование REST событий в MyEventProvider для поиска события пропущенного звонка

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,3 +1,38 @@
 <?php
 
+\Bitrix\Main\Loader::includeModule('rest');
+
+class MyEventProvider extends \Bitrix\Rest\Event\ProviderOAuth
+{
+    public function send(array $queryData)
+    {
+        $logFile = $_SERVER['DOCUMENT_ROOT'] . '/missed_call_debug.log';
+        $http = new \Bitrix\Main\Web\HttpClient();
+
+        foreach ($queryData as $key => $item)
+        {
+            if (preg_match('/192\.168\./', $item['query']['QUERY_URL']))
+            {
+                // Лог всех событий для отладки — удалить после определения нужного события
+                file_put_contents(
+                    $logFile,
+                    date('Y-m-d H:i:s') . ' | event=' . ($item['query']['QUERY_DATA']['event'] ?? 'none')
+                    . ' | data=' . json_encode($item['query']['QUERY_DATA'], JSON_UNESCAPED_UNICODE) . "\n",
+                    FILE_APPEND
+                );
+
+                $http->post($item['query']['QUERY_URL'], $item['query']['QUERY_DATA']);
+                unset($queryData[$key]);
+            }
+        }
+
+        if (count($queryData) > 0)
+        {
+            parent::send(array_values($queryData));
+        }
+    }
+}
+
+\Bitrix\Rest\Event\Sender::setProvider(MyEventProvider::instance());
+
 require_once __DIR__ . '/include/crm_missed_call_lead.php';
